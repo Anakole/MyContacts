69ee7bcfeccf7570c7e9f66285ae832c6bfc7076
Anakole <104356239+Anakole@users.noreply.github.com>
2023-01-28 00:32:51 +0200

    Create FormContainer.styled.jsx

---
+++ src/pages/FormContainer.styled.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const FormContainer = styled.div`
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 30px;
+
+  h1 {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 500;
+  }
+`;
