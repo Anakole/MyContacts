723cbc050e2026c0ab09193a3a25d5e680d12de5
Anakole <104356239+Anakole@users.noreply.github.com>
2023-01-28 00:33:06 +0200

    Create Home.jsx

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { HomeBox } from './Home.styled';
+
+const Home = () => {
+  return (
+    <HomeBox>
+      <h1>Welcome!</h1>
+      <p>To start working with the application, log in or register.</p>
+    </HomeBox>
+  );
+};
+
+export default Home;
