1a42c3a88cba19d7b4e7500c0e1a3f2c3229cad6
Anakole <104356239+Anakole@users.noreply.github.com>
2023-01-28 00:33:08 +0200

    Create Home.styled.jsx

---
+++ src/pages/Home/Home.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const HomeBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 60px;
+
+  h1 {
+    color: #1f260f;
+  }
+
+  p {
+    text-align: center;
+    width: 350px;
+    font-size: 20px;
+  }
+`;
