57d0ba99824e8afac90ea0c45a264460f181e5ca
Anakole <104356239+Anakole@users.noreply.github.com>
2023-01-28 00:32:48 +0200

    Update Contacts.styled.jsx

--- src/pages/Contacts/Contacts.styled.jsx
+++ src/pages/Contacts/Contacts.styled.jsx
@@ -2,21 +2,11 @@ import styled from '@emotion/styled';
 
 export const Box = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-
-export const PhonebookTitle = styled.h1`
-  color: #3d3d3d;
-  text-transform: uppercase;
-  letter-spacing: 1.5px;
-`;
-export const ContactsTitle = styled.h2`
-  color: #3d3d3d;
-  text-transform: uppercase;
-  font-size: 16px;
-  letter-spacing: 1.5px;
+  flex-direction: column;
+  padding-top: 60px;
+  gap: 30px;
 `;
 
 export const NoContacts = styled.p`
@@ -25,7 +15,6 @@ export const NoContacts = styled.p`
   justify-content: center;
   width: 400px;
   height: 60px;
-  color: #3d3d3d;
+  color: #1f260f;
   font-weight: 600;
-  border: 3px solid #3d3d3dab;
 `;
