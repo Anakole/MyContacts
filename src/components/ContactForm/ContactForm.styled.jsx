e78704c11739c972dd09b08418a763e8dd69da90
Anakole <104356239+Anakole@users.noreply.github.com>
2023-01-28 00:32:16 +0200

    Update ContactForm.styled.jsx

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -8,7 +8,8 @@ export const Form = styled.form`
   width: 350px;
   padding: 30px;
   margin-bottom: 30px;
-  background-color: #d8e7e7;
+  background-color: #1f260f;
+  border-radius: 8px;
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 `;
 
@@ -22,23 +23,34 @@ export const Label = styled.label`
 export const Input = styled.input`
   width: 250px;
   height: 20px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #1f260f;
+  font-size: 18px;
+  background: #f2e863b7;
+  border: 2px solid #f1edb3;
+  border-radius: 8px;
+  padding: 10px;
+  margin-top: 5px;
   margin-left: 10px;
 `;
 
 export const Button = styled.button`
-  width: 120px;
-  height: 30px;
-  margin: auto;
-  border: none;
-  background-color: #3d3d3d;
+  font-family: 'Montserrat', sans-serif;
+  color: #1f260f;
+  width: 150px;
   text-transform: uppercase;
-  font-size: 12px;
-  color: #d8e7e7;
+  background-color: #828c32;
+  padding: 10px;
+  border: none;
+  border-radius: 8px;
+  font-weight: 500;
 
   &:hover,
-  &:active {
-    background-color: white;
-    color: #3d3d3d;
-    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  &:focus {
+    background-color: #f2e963;
   }
 `;
