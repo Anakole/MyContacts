e3db2e754e5228b61994f0f910d0d2808911fe44
Anakole <104356239+Anakole@users.noreply.github.com>
2023-01-28 00:31:31 +0200

    Create AppBar.styled.jsx

---
+++ src/components/AppBar/AppBar.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 200px;
+  background: #1f260f;
+`;
